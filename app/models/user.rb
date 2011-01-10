46c4526993ad2968e1726d91b44a46a24ed8cfc8
Tobias Eilert <tobias.eilert@tux-mail.de>
2011-01-10 21:33:00 +0100

    Done with sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,10 +36,19 @@ class User < ActiveRecord::Base
     encrypted_password == encrypt(submitted_password)
   end
   
-  def self.authenticate(email, submitted_password)
+  def User.authenticate(email, submitted_password)
     user = find_by_email email
     return nil if user.nil?
     return user if user.has_password? submitted_password
+  end
+  
+  def User.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    if user && user.salt == cookie_salt
+      user
+    else
+      nil
+    end
   end
   
   private
